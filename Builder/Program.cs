1610cd99fa75aba9813a1b611caebc48de98a816
Emrah ESEN <emrhesen@gmail.com>
2019-01-16 22:09:10 +0300

    Builder design pattern added

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Builder
 {
@@ -6,8 +7,131 @@ namespace Builder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            VehicleBuilder builder;
+
+
+            Shop shop = new Shop();
+
+            builder = new CarBuilder();
+            shop.Construct(builder);
+            builder.GetVehicle.Show();
+
+            builder = new MotorCycleBuilder();
+            shop.Construct(builder);
+            builder.GetVehicle.Show();
+
+            Console.ReadKey();
+        }
+    }
+
+    // Director Class
+    class Shop
+    {
+        public void Construct(VehicleBuilder vehicleBuilder)
+        {
+            vehicleBuilder.BuildFrame();
+            vehicleBuilder.BuildEngine();
+            vehicleBuilder.BuildWheels();
+            vehicleBuilder.BuildDoors();
+        }
+    }
+
+    // Abstract Builder Class
+    abstract class VehicleBuilder
+    {
+        protected Vehicle vehicle;
+
+        public Vehicle GetVehicle { get { return vehicle; } }
+
+        public abstract void BuildFrame();
+        public abstract void BuildEngine();
+        public abstract void BuildWheels();
+        public abstract void BuildDoors();
+    }
+
+    // Concrete Class
+    class MotorCycleBuilder : VehicleBuilder
+    {
+        public MotorCycleBuilder()
+        {
+            vehicle = new Vehicle("MotorCycle");
+        }
+
+        public override void BuildDoors()
+        {
+            vehicle["doors"] = "0";
+        }
+
+        public override void BuildEngine()
+        {
+            vehicle["engine"] = "500cc"; 
+        }
+
+        public override void BuildFrame()
+        {
+            vehicle["frame"] = "MotorCycle Frame";
+        }
+
+        public override void BuildWheels()
+        {
+            vehicle["wheels"] = "2";
+        }
+    }
+
+    // Concrete Class
+    class CarBuilder : VehicleBuilder
+
+    {
+        public CarBuilder()
+        {
+            vehicle = new Vehicle("Car");
+        }
+
+        public override void BuildFrame()
+        {
+            vehicle["frame"] = "Car Frame";
+        }
+
+        public override void BuildEngine()
+        {
+            vehicle["engine"] = "2500 cc";
+        }
+
+        public override void BuildWheels()
+        {
+            vehicle["wheels"] = "4";
+        }
+
+        public override void BuildDoors()
+        {
+            vehicle["doors"] = "4";
+        }
+    }
+
+    class Vehicle
+    {
+        private string _vehicleType;
+        private Dictionary<string, string> _parts = new Dictionary<string, string>();
+
+        public Vehicle(string vehicleType)
+        {
+            this._vehicleType = vehicleType;
+        }
+
+        public string this[string key]
+        {
+            get { return _parts[key]; }
+            set { _parts[key] = value; }
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("\n---------------------------");
+            Console.WriteLine("Vehicle Type: {0}", _vehicleType);
+            Console.WriteLine(" Frame : {0}", _parts["frame"]);
+            Console.WriteLine(" Engine : {0}", _parts["engine"]);
+            Console.WriteLine(" #Wheels: {0}", _parts["wheels"]);
+            Console.WriteLine(" #Doors : {0}", _parts["doors"]);
         }
     }
 }
