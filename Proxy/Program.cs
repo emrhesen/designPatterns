9ede4e2d119f11a7ea2b8081e492bb0e250ad1c1
Emrah ESEN <emrhesen@gmail.com>
2019-09-15 19:39:15 +0300

    Proxy pattern

---
+++ Proxy/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IProductService _productService = new ProductService();
+            IProductService _productService = new ProductServiceProxy(new ProductService());
+
+            Console.WriteLine($"Product Count {_productService.GetProduct()}");
+
+            Console.ReadLine();
+        }
+    }
+
+    public interface IProductService
+    {
+        int GetProduct();
+    }
+
+    public class ProductService : IProductService
+    {
+        public int GetProduct()
+        {
+            Console.WriteLine("Real Service Object");
+            return 5;
+        }
+    }
+
+    public class ProductServiceProxy : IProductService
+    {
+        private readonly IProductService _productService;
+
+        public ProductServiceProxy(IProductService productService)
+        {
+            //TO-DO : IoC Container
+            _productService = new ProductService();
+        }
+
+        public int GetProduct()
+        {
+            Console.WriteLine("Proxy Service Object");
+            return _productService.GetProduct();
+        }
+    }
+}
