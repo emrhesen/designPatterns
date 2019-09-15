6b5272f3de69a533f6b674809cfb7464cff6c036
Emrah ESEN <emrhesen@gmail.com>
2019-09-15 14:20:53 +0300

    adapter design pattern

---
+++ Adapter/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IProductService productService = new ProductService();
+            IProductService productService = new AxaptaProductServiceAdapter();
+
+            productService.ListProduct();
+
+            Console.ReadLine();
+        }
+
+    }
+
+    interface IProductService
+    {
+        void ListProduct();
+    }
+
+
+    class ProductService : IProductService
+    {
+        public void ListProduct()
+        {
+            Console.WriteLine("Products from database");
+        }
+    }
+
+    // Adapter Class
+    class AxaptaProductServiceAdapter : IProductService
+    {
+        public void ListProduct()
+        {
+            var axapta = new AxaptaProductGetter();
+            axapta.GetProductList();
+        }
+    }
+
+
+    // Adaptee
+    // External Service , DLL , Nuget ...etc (This class close World for us)
+    class AxaptaProductGetter
+    {
+        public void GetProductList()
+        {
+            Console.WriteLine("Products from axapta");
+        }
+    }
+
+
+}
